5730ea75b470369891028790ba36f5bb47897ea9
v_akymov <vladyslav.akimov@ralabs.org>
2025-01-12 01:52:37 +0200

    change Elixir version for workflow

--- test_crud_jt_package/mix.exs
+++ test_crud_jt_package/mix.exs
@@ -5,7 +5,7 @@ defmodule TestCrudJtPackage.MixProject do
     [
       app: :test_crud_jt_package,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
